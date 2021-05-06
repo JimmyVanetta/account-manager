29d9673f81fbc4088a7da73601cc9194fefa2081
Jimmy Vanetta <jvanetta@TRACKER.local>
2021-05-06 08:14:12 -0400

    re-structured folders

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import store from './store'
-import { routes } from './route'
+import { routes } from './router/route'
 
 Vue.config.productionTip = false
 Vue.use(VueRouter)
